ae8f5105c31d62381745a8a124fbf5dea4418f1a
aresu-1704 <aresu.work1704@gmail.com>
2025-06-07 14:16:50 +0700

    Đăng nhập, đăng ký, JWT

--- backend/backend/Services/UsersService.cs
+++ backend/backend/Services/UsersService.cs
@@ -1,27 +1,29 @@
-﻿using backend.Models;
-using backend.Repositories;
+﻿using backend.Interfaces;
+using backend.Models;
 using backend.Utils.Securities;
+using MongoDB.Driver;
 
 namespace backend.Services
 {
     public class UsersService : IUsersService
     {
         private readonly IUserRepository _usersRepository;
-
-        public UsersService(IUserRepository usersRepository)
+        private readonly IJWTService _jwtService;
+        public UsersService(IUserRepository usersRepository, IJWTService jwtService)
         {
             _usersRepository = usersRepository;
+            _jwtService = jwtService;
         }
 
-        public async Task<(string id, int role)> VerifyLogin(string username, string password)
+        public async Task<string?> VerifyLogin(string username, string password)
         {
             var loginUser = await _usersRepository.GetByUsernameAsync(username);
 
             if(loginUser != null)
             {
-                if(loginUser.Status == 0)
+                if(loginUser.Status == false)
                 {
-                    return (loginUser.Id, -1); //Tài khoản đã bị khóa
+                    return "Tài khoản đã bị khóa";
                 }
 
                 var salt = loginUser.Salt;
@@ -31,22 +33,57 @@ namespace backend.Services
 
                 if(hashPassword == storePassword)
                 {
-                    return (loginUser.Id, loginUser.Role);
+                    return _jwtService.GenerateJwtToken(loginUser.Id, loginUser.Name, loginUser.Role.ToString()); ;
                 }
                 else
                 {
-                    return (null, -1);
+                    return null;
                 }
             }
             else
             {
-                return (null, -1);
+                return null;
             }
         }
 
-        public async Task<string> Register(Users user)
+        public async Task<string> Register(string username, string fullname, string email, string password, string phoneNumber, DateTime dateOfBirth, int gender)
         {
-            return "Register succesfully.";
+            try
+            {
+                var salt = HashingUtil.GenerateSalt(16);
+                var hashPassword = HashingUtil.HashPassword(password, salt);
+
+                Users registerUser = new Users()
+                {
+                    Username = username,
+                    Name = fullname,
+                    Email = email,
+                    Password = hashPassword,
+                    PhoneNumber = phoneNumber,
+                    DateOfBirth = dateOfBirth,
+                    Salt = salt,
+                    Gender = gender,
+                    CreatedAt = DateTime.UtcNow,
+                    Status = true,
+                    LastLogin = null,
+                    Role = 1,
+                    IsEmailVerified = false,
+                    IsPhoneVerified = false,
+                    AvatarUrl = null,
+                };
+
+                await _usersRepository.CreateAsync(registerUser);
+                return "Đăng ký thành công.";
+            }
+            catch (MongoWriteException ex) when (ex.WriteError.Category == ServerErrorCategory.DuplicateKey)
+            {
+                return "Email đã tồn tại.";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Đã xảy ra lỗi " + ex.ToString());
+                return "Đăng ký thất bại.";
+            }
         }
         //Task UpdateAsync(string id, Users user);
         //Task DeleteAsync(string id);

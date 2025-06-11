e35f21bcab04e80c6f80c651d302152566858957
huytn593 <huytn593@gmail.com>
2025-06-11 19:11:11 +0700

    Xong HomePage.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,41 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-<<<<<<< Updated upstream
-=======
-//Điều hướng
->>>>>>> Stashed changes
-import SignInPage from './pages/SignIn/SignInPage'
-import SignUpPage from './pages/SignUp/SignUpPage'
+// Điều hướng
+import SignInPage from './pages/SignIn/SignInPage';
+import SignUpPage from './pages/SignUp/SignUpPage';
 import HomePage from './pages/Home/HomePage';
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 import { ToastContainer } from 'react-toastify/unstyled';
-import {AuthProvider} from "./context/AuthContext";
+import { AuthProvider } from './context/AuthContext';
 
 const App = () => (
-  <Router>
-      <AuthProvider>
-          <MainLayout>
-              <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  {/* <Route path="/songs" element={<SongsPage />} />
-<<<<<<< Updated upstream
-          <Route path="/albums" element={<AlbumsPage />} /> */}
-=======
-                    <Route path="/albums" element={<AlbumsPage />} /> */}
-
->>>>>>> Stashed changes
-                  <Route path="/signin" element={<SignInPage />} />
-                  <Route path="/signup" element={<SignUpPage />} />
-              </Routes>
-              <ToastContainer />
-          </MainLayout>
-      </AuthProvider>
-  </Router>
+    <Router>
+        <AuthProvider>
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    {/* <Route path="/songs" element={<SongsPage />} /> */}
+                    {/* <Route path="/albums" element={<AlbumsPage />} /> */}
+                    <Route path="/signin" element={<SignInPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                </Routes>
+                <ToastContainer />
+            </MainLayout>
+        </AuthProvider>
+    </Router>
 );
 
 export default App;

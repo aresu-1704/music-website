26cb604a58a037a3d75e581b4d10e89847998129
aresu-1704 <aresu.work1704@gmail.com>
2025-06-12 19:38:09 +0700

    Tích hợp API thanh toán VNPay

---
+++ backend/backend/Interfaces/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using backend.Controllers;
+
+namespace backend.Interfaces
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        Task<PaymentResponseModel> PaymentExecute(IQueryCollection collections);
+    }
+}
